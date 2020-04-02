383dcacefd87841995cc3d8ded1a34f1202d9ad5
carmine <hl13886803841@sina.com>
2020-04-02 18:23:40 +0800

    点击播放按钮可以播放歌曲了，再次点击可以暂停播放，但是图标没有改变

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,7 +1,6 @@
 import './icons.js'
 //http://carminepink.xyz/data-mock/huawei-music/music-list.json
 //https://jirengu.github.io/data-mock/huawei-music/music-list.json
-console.log('hi')
 
 const $ = (selector) => {
    document.querySelector(selector)
@@ -12,21 +11,41 @@ const $$ = (selector) => {
 
 class Player {
    constructor(node) {
-      this.root = typeof node === 'string' ? $(node) : node
+      this.root = typeof node === 'string' ? document.querySelector(node) : node
       this.songList = []
       this.currenIndex = 0
+      this.audio = new Audio()
       this.start()
+      this.bind()
    }
 
-   bind() {
-   }
-   start(){
+   start() {
       fetch('http://carminepink.xyz/data-mock/huawei-music/music-list.json')
-         .then(res=>res.json())
-         .then(data=>{
+         .then(res => res.json())
+         .then(data => {
             console.log(data)
             this.songList = data
          })
+   }
+
+   bind() {
+      let self = this
+      const playOrPauseButton = this.root.querySelector('.btn-play-pause')
+      playOrPauseButton.onclick = function () {
+         self.audio.src = self.songList[self.currenIndex].url
+         if (this.classList.contains('playing')) {
+            self.audio.pause()
+            this.classList.remove('playing')
+            this.classList.add('pause')
+         } else if (this.classList.contains('pause')) {
+            self.audio.play()
+            this.classList.remove('pause')
+            this.classList.add('playing')
+         }
+      }
+   }
+
+   playSong() {
    }
 }
 
